fa52ceaed4136a70e2c11e410154ddbf42534005
LamThanhNguyen <dp1.nguyenthanhlam@gmail.com>
2025-06-20 10:46:58 +0700

    update invoice flow

--- server_main/api/invoice.go
+++ server_main/api/invoice.go
@@ -1,6 +1,11 @@
 package api
 
-import "time"
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"time"
+)
 
 // Invoice describes the JSON payload sent to the microservice
 type Invoice struct {
@@ -15,6 +20,26 @@ type Invoice struct {
 	CreatedAt time.Time `json:"created_at"`
 }
 
+// callInvoiceMicro calls the invoicing microservice
 func (server *Server) callInvoiceMicro(inv Invoice) error {
+	url := "http://localhost:5000/invoice/create-and-send"
+	out, err := json.MarshalIndent(inv, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(out))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
 	return nil
 }
